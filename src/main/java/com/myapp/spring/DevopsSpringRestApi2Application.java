33cdf4ec8af5f192e2feef53e3bd72e4a60dd1e1
pragathihebbarkm <hebbarkmpragathi>
2022-02-01 15:45:42 +0530

    scripted pipeline

---
+++ src/main/java/com/myapp/spring/DevopsSpringRestApi2Application.java
@@ -0,0 +1,15 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class DevopsSpringRestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsSpringRestApi2Application.class, args);
+	}
+
+	
+	
+}
---
+++ src/main/java/com/myapp/spring/web/MyRestApi.java
@@ -0,0 +1,22 @@
+package com.myapp.spring.web;
+
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MyRestApi {
+	
+	@GetMapping
+	public String SayHello() {
+		return "Welcome To World Of Rest Api With Java Spring and Current Date "+LocalDate.now();
+	}
+	@GetMapping("/hello")
+	public String SayHello1() {
+		return "Welcome To World Of Rest Api With Java1 Spring and Current Date "+LocalDate.now();
+	}
+
+}
